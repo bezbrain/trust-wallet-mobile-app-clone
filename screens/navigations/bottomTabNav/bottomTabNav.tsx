22aa1a4597e60f516982277d86389c3d1050706d
bezbrain <belloquadriolawale@gmail.com>
2024-04-22 21:27:12 +0100

    Add the bottom tab

--- screens/navigations/bottomTabNav/bottomTabNav.tsx
+++ screens/navigations/bottomTabNav/bottomTabNav.tsx
@@ -2,24 +2,69 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WalletPreview from "../../walletPreview";
 import { Feather } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import {
+  discoverOptions,
+  earnOptions,
+  homeOptions,
+  swapOptions,
+} from "./tabOptions/tabOptions";
 
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabNav = () => {
   return (
     <BottomTab.Navigator
-      screenOptions={{
-        tabBarIcon: ({ color, size }) => {
-          return <Feather name="home" size={size} color={color} />;
-        },
-      }}
+    //   screenOptions={}
     >
       <BottomTab.Screen
         name="WalletHome"
         component={WalletPreview}
         options={{
-          title: "Home",
-          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="home" size={size} color={color} />;
+          },
+          ...homeOptions,
+        }}
+      />
+      <BottomTab.Screen
+        name="Swap"
+        component={WalletPreview}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="swap" size={size} color={color} />;
+          },
+          ...swapOptions,
+        }}
+      />
+      <BottomTab.Screen
+        name="Earn"
+        component={WalletPreview}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <MaterialCommunityIcons
+                name="vector-circle"
+                size={size}
+                color={color}
+              />
+            );
+          },
+          ...earnOptions,
+        }}
+      />
+      <BottomTab.Screen
+        name="Discover"
+        component={WalletPreview}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <AntDesign name="codepen-circle" size={size} color={color} />
+            );
+          },
+          ...discoverOptions,
         }}
       />
     </BottomTab.Navigator>
--- screens/navigations/bottomTabNav/tabOptions/tabOptions.ts
+++ screens/navigations/bottomTabNav/tabOptions/tabOptions.ts
@@ -1,3 +1,23 @@
 export const generalTabOptions = {
   //
 };
+
+export const homeOptions = {
+  title: "Home",
+  tabBarLabel: "Home",
+};
+
+export const swapOptions = {
+  title: "Swap",
+  tabBarLabel: "Swap",
+};
+
+export const earnOptions = {
+  title: "Earn",
+  tabBarLabel: "Earn",
+};
+
+export const discoverOptions = {
+  title: "Discover",
+  tabBarLabel: "Discover",
+};
